a0e6a822cc004d8465b852402f46c69c0c9498b2
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2018-12-11 18:39:43 +0100

    Added SERVER_FILE_DISPLAY_NAME setting. Improved on the structure and look of the settings file a bit.

--- ModpackUploadTool.bat
+++ ModpackUploadTool.bat
@@ -10,7 +10,7 @@ ECHO Making manifest...
 twitch_export-win.exe -d "%cd%" -n "%CLIENT_FILENAME%" -p "%MODPACK_VERSION%" -c "%cd%/.build.json" -ct "%CURSEFORGE_TOKEN%"
 
 ECHO Setting up metadata for uploading...
-SET CLIENT_METADATA="{'changelog': '%CLIENT_CHANGELOG%','changelogType': '%CLIENT_CHANGELOG_TYPE%','displayName': '%FILE_DISPLAY_NAME%','gameVersions': %GAME_VERSIONS%,'releaseType': '%CLIENT_RELEASE_TYPE%'}"
+SET CLIENT_METADATA="{'changelog': '%CLIENT_CHANGELOG%','changelogType': '%CLIENT_CHANGELOG_TYPE%','displayName': '%CLIENT_FILE_DISPLAY_NAME%','gameVersions': %GAME_VERSIONS%,'releaseType': '%CLIENT_RELEASE_TYPE%'}"
 ECHO Uploading Modpack to https://minecraft.curseforge.com/api/projects/%CURSEFORGE_PROJECT_ID%/upload-file
 curl --user %USER%:%CURSEFORGE_TOKEN% -H "Accept: application/json" -H X-Api-Token:%CURSEFORGE_TOKEN% -F metadata=%CLIENT_METADATA% -F file=@%CLIENT_FILENAME%-%MODPACK_VERSION%.zip https://minecraft.curseforge.com/api/projects/%CURSEFORGE_PROJECT_ID%/upload-file >> ModpackFileId.json
 
@@ -20,7 +20,7 @@ SET FILE_ID=%FILE_ID:{"id":=%
 SET FILE_ID=%FILE_ID:}=%
 CALL :TRIM %FILE_ID% FILE_ID
 ECHO ID Returned: %FILE_ID%
-del ModpackFileId.json
+DEL ModpackFileId.json
 
 :: Create server files
 ECHO Making server files...
@@ -28,7 +28,7 @@ ECHO Making server files...
 
 :: Uploading Server Files...
 ECHO Setting up metadata for uploading...
-SET SERVER_METADATA="{'changelog': '%SERVER_CHANGELOG%','changelogType': '%SERVER_CHANGELOG_TYPE%','displayName': '%FILE_DISPLAY_NAME%','parentFileId': %FILE_ID%,'releaseType': '%SERVER_RELEASE_TYPE%'}"
+SET SERVER_METADATA="{'changelog': '%SERVER_CHANGELOG%','changelogType': '%SERVER_CHANGELOG_TYPE%','displayName': '%SERVER_FILE_DISPLAY_NAME%','parentFileId': %FILE_ID%,'releaseType': '%SERVER_RELEASE_TYPE%'}"
 ECHO Uploading Modpack to https://minecraft.curseforge.com/api/projects/%CURSEFORGE_PROJECT_ID%/upload-file
 curl --user %USER%:%CURSEFORGE_TOKEN% -H "Accept: application/json" -H X-Api-Token:%CURSEFORGE_TOKEN% -F metadata=%SERVER_METADATA% -F file=@%SERVER_FILENAME%-%MODPACK_VERSION%.zip https://minecraft.curseforge.com/api/projects/%CURSEFORGE_PROJECT_ID%/upload-file >> ModpackFileId.json
 
