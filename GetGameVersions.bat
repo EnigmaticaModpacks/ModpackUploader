20db6bb942a1ab132c77d30ebdf915261f57b1f4
NielsPilgaard <niels.pilgaard@hotmail.com>
2019-02-02 20:10:03 +0100

    GetGameVersions now requires a token due (pressumably) to CurseForge API changes. It will use the token provided in the modpack_upload_settings.cfg

--- GetGameVersions.bat
+++ GetGameVersions.bat
@@ -1,2 +1,7 @@
 @ECHO OFF
-curl https://minecraft.curseforge.com/api/game/versions >> GameVersions.json
+
+:: Load Settings from config
+ECHO Loading variables from modpack_upload_settings.cfg
+for /f "delims==; tokens=1,2 eol=;" %%G in (modpack_upload_settings.cfg) do set %%G=%%H
+
+curl -H X-Api-Token:%CURSEFORGE_TOKEN% https://minecraft.curseforge.com/api/game/versions >> GameVersions.json
