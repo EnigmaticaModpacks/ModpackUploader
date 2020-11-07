2a10c7dd216e6b208781feb29fa1456dd6c30b8f
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-11-07 19:39:16 +0100

    windows fixes, formatting

--- tools/ModpackDownloader.ps1
+++ tools/ModpackDownloader.ps1
@@ -1,12 +1,13 @@
-Set-Location ".."
-. ./settings.ps1
+Set-Location "$PSScriptRoot\.."
+
+. .\settings.ps1
 
 function Download-GithubRelease {
     param(
-        [parameter(Mandatory=$true)]
+        [parameter(Mandatory = $true)]
         [string]
         $repo,
-        [parameter(Mandatory=$true)]
+        [parameter(Mandatory = $true)]
         [string]
         $file
     )
@@ -39,31 +40,31 @@ function Clear-SleepHost {
 }
 if ($IsLinux) {
     #Lets Check if the user has Curl Installed
-    if (-not (test-path "/usr/bin/curl")) {Write-Host "Curl needed to use the ModpackUploader."}
-        Set-Alias cl "curl"
+    if (-not (test-path "/usr/bin/curl")) { Write-Host "Curl needed to use the ModpackUploader." }
+    Set-Alias cl "curl"
 
     #If Program is NOT installed KILL
-    if (-not (test-path "/usr/bin/curl")) {Break}
+    if (-not (test-path "/usr/bin/curl")) { Break }
 }
 elseif ($IsWindows) {
     #Lets Check if the user has 7-Zip Installed
-    if (-not (test-path "$env:C:\Windows\System32\curl.exe")) {Write-Host "Curl needed to use the ModpackUploader."}
-        Set-Alias cl "$env:C:\Windows\System32\curl.exe"
+    if (-not (test-path "$env:C:\Windows\System32\curl.exe")) { Write-Host "Curl needed to use the ModpackUploader." }
+    Set-Alias cl "$env:C:\Windows\System32\curl.exe"
 
     #If Program is NOT installed KILL
-    if (-not (test-path "$env:C:\Windows\System32\curl.exe")) {Break}
+    if (-not (test-path "$env:C:\Windows\System32\curl.exe")) { Break }
 }
 
 #Download the Mod Pack Downloader Tool
-if (!(Test-Path ModpackDownloader.jar) -or $ENABLE_ALWAYS_UPDATE_JARS) {
-Write-Host "######################################" -ForegroundColor Cyan
-Write-Host ""
-Write-Host "Downloading Modpack Downloader...     " -ForegroundColor Green
-Write-Host ""
-Write-Host "######################################" -ForegroundColor Cyan
-Download-GithubRelease -repo "CrankySupertoon/ModPackDownloader" -file $ModpackDownloaderDL
-New-Item "./tools" -ItemType "directory" -Force -ErrorAction SilentlyContinue
-Move-Item -Path "$ModpackDownloaderDL" -Destination "tools/ModpackDownloader.jar"
+if (!(Test-Path "ModpackDownloader.jar") -or $ENABLE_ALWAYS_UPDATE_JARS) {
+    Write-Host "######################################" -ForegroundColor Cyan
+    Write-Host ""
+    Write-Host "Downloading Modpack Downloader...     " -ForegroundColor Green
+    Write-Host ""
+    Write-Host "######################################" -ForegroundColor Cyan
+    Download-GithubRelease -repo "CrankySupertoon/ModPackDownloader" -file $ModpackDownloaderDL
+    New-Item "./tools" -ItemType "directory" -Force -ErrorAction SilentlyContinue
+    Move-Item -Path "$ModpackDownloaderDL" -Destination "tools/ModpackDownloader.jar"
 }
 
 #Now lets download the mods
@@ -72,4 +73,4 @@ Write-Host ""
 Write-Host "Downloading Mods...                   " -ForegroundColor Green
 Write-Host ""
 Write-Host "######################################" -ForegroundColor Cyan
-java -jar tools/ModpackDownloader.jar -manifest mods.json -folder mods
+java -jar "tools/ModpackDownloader.jar" -manifest "mods.json" -folder "mods"
