b0b78f30cf3551364018177d2c2c1986ca74d751
Nora <norahanegan@gmail.com>
2021-03-22 12:34:13 -0400

    secrets: add

---
+++ secrets.ps1
@@ -0,0 +1,8 @@
+# Settings for the ModpackUploader
+# For details/help see: https://github.com/NillerMedDild/ModpackUploader
+
+# =====================================================================//
+#  CURSEFORGE ACCOUNT SETTINGS
+# =====================================================================//
+
+$CURSEFORGE_TOKEN = "My-Token"
