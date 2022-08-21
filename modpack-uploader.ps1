03ed0311fd8219921a3b9ee2a4dbea166cdf098d
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2022-08-21 18:51:31 +0200

    Added logging when making modlist

--- modpack-uploader.ps1
+++ modpack-uploader.ps1
@@ -379,6 +379,10 @@ function Update-Modlist {
             Get-GitHubRelease -repo "ModdingX/ModListCreator" -file $MODLIST_CREATOR_JAR
         }
 
+        Write-Host
+        Write-Host "Generating Modlist..."
+        Write-Host
+	
         Remove-Item $MODLIST_PATH -ErrorAction SilentlyContinue
         java -jar $MODLIST_CREATOR_JAR modlist --output $MODLIST_PATH --detailed "$CLIENT_ZIP_NAME.zip"
         Copy-Item -Path $MODLIST_PATH -Destination "$INSTANCE_ROOT/MODLIST.md"
